29e988bf49bcf6b0a21c8d458edb4bc8f9481dca
fredpaiva42 <fredericogdpaiva@gmail.com>
2021-12-02 18:57:39 -0300

    Adicionando condições.

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,10 +6,23 @@ int main() {
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("********************************************\n");
 
-    printf("Qual o seu chute? ");
+    printf("Qual o seu chute?\n");
     scanf("%d", &chute);
 
-    printf("O seu chute foi: %d", chute);
+    printf("O seu chute foi: %d\n", chute);
+
+    if(chute == numeroSecreto){
+        printf("Parabéns! Você acertou!\n");
+    }else{
+        if(chute < numeroSecreto){
+            printf("Você errou!\n");
+            printf("Seu chute foi menor que o número secreto, tente de novo!\n");
+        }else{
+            printf("Você errou!\n");
+            printf("Seu chute foi maior que o número secreto, tente de novo!\n");
+        }
+
+    }
 
     return 0;
 }
