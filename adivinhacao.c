73c60490f9ab9ac3a1755b91071d3d2b1844fc5f
fredpaiva42 <fredericogdpaiva@gmail.com>
2021-12-03 18:45:34 -0300

    Limitando tentativas

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int numeroSecreto = 42, chute;
+    int numeroSecreto = 42, chute, i;
     printf("********************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("********************************************\n");
 
-    printf("Qual o seu chute?\n");
-    scanf("%d", &chute);
+    for(i=1; i<= 3; i++) {
+        printf("Tentativa %d de 3\n", i);
+        printf("Qual o seu chute?\n");
+        scanf("%d", &chute);
 
-    printf("O seu chute foi: %d\n", chute);
+        printf("O seu chute foi: %d\n", chute);
 
-    if(chute == numeroSecreto){
-        printf("Parabéns! Você acertou!\n");
-    }else{
-        if(chute < numeroSecreto){
-            printf("Você errou!\n");
-            printf("Seu chute foi menor que o número secreto, tente de novo!\n");
-        }else{
-            printf("Você errou!\n");
-            printf("Seu chute foi maior que o número secreto, tente de novo!\n");
+        int acertou = (chute == numeroSecreto);
+
+        if (acertou) {
+            printf("Parabéns! Você acertou!\n");
+        } else {
+
+            int maior = chute > numeroSecreto;
+            if (maior) {
+                printf("Seu chute foi maior que o número secreto, tente de novo!\n");
+            } else {
+                printf("Seu chute foi menor que o número secreto, tente de novo!\n");
+            }
         }
-
     }
-
+    printf("Fim de Jogo!\n");
     return 0;
 }
